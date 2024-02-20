05312dc2a6e63d32e7e8864c8dae233c4972830f
stephanie-niva <stephanienivaa@gmail.com>
2024-02-20 15:05:23 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+fun main(){
+var num1 = 25
+    var num2 = 10
+    var result = num1 + num2
+    println(result)
+    var num3 =8
+    var num4= 4
+    var ans= num3 % num4
+    println(ans)
+    var a = 8
+    var b = 5
+    var c = a + b
+    println(c)
+    var d= 8
+    var e = 5
+    d += e
+    println(d)
+    var x = 8
+    var y = 5
+    x *= y
+    println(x)
+    var s = 8
+    s++
+    var w = 8
+    w--
+    println(w)
+    var q = 8
+    var f = 5
+    println(8>=5)
+    var ab = 8
+    var bc = 5
+    println(ab<=bc)
+var ac = 8
+    var dc = 5
+    println(ac==dc)
+    var az = 8
+    var ad = 5
+    println(a!=b)
+
+}
